c8fb83a2d2a6f6fe5ac3fd9fe43debb39a74dd20
YashGoyal89 <yashgoyal9879@gmail.com>
2025-07-25 11:09:41 +0530

    java backend updated

--- src/backend/Java/auth-service/src/main/java/com/internsync/auth/AuthServiceApplication.java
+++ src/backend/Java/auth-service/src/main/java/com/internsync/auth/AuthServiceApplication.java
@@ -1,11 +1,17 @@
 package com.internsync.auth;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.internsync.auth.*")
+@EnableAutoConfiguration
 public class AuthServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(AuthServiceApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
 }
